e21402908a18c4fb78b6403860c02b297d1726ef
Hient <hient@usf.edu>
2025-02-16 15:24:46 -0500

    Editing words error

--- cc_8.js
+++ cc_8.js
@@ -2,8 +2,8 @@
 //Fuction to calculate net salary 
 function calculateSalary(baseSalary, bonus, taxRate){
     let netSalary = (baseSalary + bonus) - (baseSalary * taxRate); //Calculating net salary by adding the base salary and bonus 
-    console.log (`Net Salary: $${netSalary.toFixted(2)}`); //Logging the net salary to the console
-}
+    console.log (`Net Salary: $${netSalary.toFixed(2)}`); //Logging the net salary to the console
+};
 
 //Test cases
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
@@ -13,7 +13,7 @@ calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
 //Function expression to calculate the final price
 let calculateDiscount = function(price, discountRate){
     let finalPrice = price - (price * discountRate); //Calculating the final price by subtracting the discount
-    console.log(`Final Prive: $${finalPrice.toFixted(2)}`); //Logging the final price 
+    console.log(`Final Prive: $${finalPrice.toFixed(2)}`); //Logging the final price 
 }
 
 //Test cases
