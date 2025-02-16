77f0b8039c9e1f5f5246ff6854f939be62782398
Hient <hient@usf.edu>
2025-02-16 14:21:21 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -8,3 +8,14 @@ function calculateSalary(baseSalary, bonus, taxRate){
 //Test cases
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
+
+//Task 2: Function Expression
+//Function expression to calculate the final price
+let calculateDiscount = function(price, discountRate){
+    let finalPrice = price - (price * discountRate); //Calculating the final price by subtracting the discount
+    console.log(`Final Prive: $${finalPrice.toFixted(2)}`); //Logging the final price 
+}
+
+//Test cases
+calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
